dae693f54a4b28ae7016edf73e67515007ba2fc3
Robot <alx.care@gmail.com>
2018-03-09 12:51:35 +0100

    refactor sender node

--- src/sender/src/sender_node.py
+++ src/sender/src/sender_node.py
@@ -3,27 +3,37 @@
 import rospy
 from sender.msg import test
 from ai_robot_status.srv import NodeReadiness
+from ai_robot_status.msg import RobotStatus
 from ai_robot_status.Services import RobotServices
 from ai_robot_status.RStatus.State import RobotState
 
-robot_status = RobotState.ROBOT_INIT
-def talker():
-    pub = rospy.Publisher('chatter', test, queue_size=10)
-    rospy.init_node('sender', anonymous=True)
-    rate = rospy.Rate(1) # 10hz
+class TalkerNode(object):
+    """docstring for TalkerNode"""
+    def __init__(self):
+        self.robot_status = RobotState.ROBOT_INIT
+        pub = rospy.Publisher('chatter', test, queue_size=10)
+        rospy.Subscriber("/ai/robot_watcher/robot_status", RobotStatus, self.get_robot_status)
+        rospy.init_node('sender', anonymous=True)
+        rate = rospy.Rate(1) # 10hz
 
-    RobotServices.service_ready("receiver", "", True)
-    while not rospy.is_shutdown():
+        RobotServices.service_ready("receiver", "", True)
+        while not rospy.is_shutdown():
+            if self.robot_status != RobotState.ROBOT_HALT:
+                msg = test()
+                msg.value = False
+                msg.name = "sender/send"
+                # rospy.loginfo(msg)
+                pub.publish(msg)
+            rate.sleep()
+        
+    
 
-        msg = test()
-        msg.value = False
-        msg.name = "sender/send"
-        # rospy.loginfo(msg)
-        pub.publish(msg)
-        rate.sleep()
+    def get_robot_status(self, msg):
+        print("callback")
+        self.robot_status = msg.robot_status
 
 if __name__ == '__main__':
     try:
-        talker()
+        TalkerNode()
     except rospy.ROSInterruptException:
         pass    
