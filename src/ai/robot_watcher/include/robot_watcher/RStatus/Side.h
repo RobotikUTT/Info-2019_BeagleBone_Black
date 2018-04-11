1059b261a8eb9cca9074bdbbed80d89455f8270b
Alexis CARE <alx.care@gmail.com>
2018-04-11 11:28:50 +0200

    change class to define

--- src/ai/robot_watcher/include/robot_watcher/RStatus/Side.h
+++ src/ai/robot_watcher/include/robot_watcher/RStatus/Side.h
@@ -1,11 +1,14 @@
 #ifndef SIDE_H
 #define  SIDE_H
-class Side
-{
-public:
-  bool GREEN     = 0;
-  bool ORANGE    = 1;
+// class Side
+// {
+// public:
+//   static const bool GREEN     = 0;
+//   static const bool ORANGE    = 1;
+//
+// };
 
-};
+#define SIDE_GREEN  0
+#define SIDE_ORANGE 1
 
 #endif
