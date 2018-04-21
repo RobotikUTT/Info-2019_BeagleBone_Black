bba13cb030fee55219552a32b558eace62f62fe3
Alexis CARE <alx.care@gmail.com>
2018-04-21 23:40:26 +0200

    define MoveType

---
+++ src/procedures/move/include/move/MoveType.h
@@ -0,0 +1,19 @@
+#ifndef MOVE_TYPE_H
+#define MOVE_TYPE_H
+
+
+// class MoveType:
+// {
+// public:
+//   uint8_t GO_TO_ANGLE = 0;
+//   uint8_t GO_TO = 1;
+//   uint8_t ROTATION = 2;
+//   uint8_t ROTATION_NO_MODULO = 3;
+//
+// };
+
+#define GO_TO_ANGLE 0
+#define GO_TO 1
+#define ROTATION 2
+#define ROTATION_NO_MODULO 3
+#endif
