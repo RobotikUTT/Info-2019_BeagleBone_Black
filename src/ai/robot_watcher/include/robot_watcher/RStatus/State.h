b4cd820ba716b0efdd8f0b04bd91930b7c0805e0
Alexis CARE <alx.care@gmail.com>
2018-04-25 17:15:36 +0200

    change to defines

--- src/ai/robot_watcher/include/robot_watcher/RStatus/State.h
+++ src/ai/robot_watcher/include/robot_watcher/RStatus/State.h
@@ -1,14 +1,19 @@
 #ifndef STATE_H
 #define STATE_H
 
-class RobotState
-{
-public:
-	uint8_t ROBOT_INIT     = 0; // nodes initializing
-	uint8_t ROBOT_READY    = 1; // all nodes ready, waiting start
-	uint8_t ROBOT_RUNNING  = 2; // robot in game
-	uint8_t ROBOT_HALT     = 3; // end of game
+// class RobotState
+// {
+// public:
+// 	uint8_t ROBOT_INIT     = 0; // nodes initializing
+// 	uint8_t ROBOT_READY    = 1; // all nodes ready, waiting start
+// 	uint8_t ROBOT_RUNNING  = 2; // robot in game
+// 	uint8_t ROBOT_HALT     = 3; // end of game
+//
+// };
 
-};
+#define ROBOT_INIT		0
+#define ROBOT_READY		1
+#define ROBOT_RUNNING	2
+#define ROBOT_HALT		3
 
 #endif
