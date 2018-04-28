6c0eecc49c4f9b086e02ba2544f817bec5db57d1
Alexis CARE <alx.care@gmail.com>
2018-04-28 20:12:52 +0200

    GroupBlocks class

---
+++ src/procedures/block/include/block/GroupBlocks.h
@@ -0,0 +1,31 @@
+#ifndef GROUP_BLOCK_H
+#define GROUP_BLOCK_H
+
+#define _USE_MATH_DEFINES
+#include "block/OrientedPoint.h"
+#include <math.h>
+#include <cmath>
+
+#define SEMI_CUBE_LENTH 29 //mm
+#define CENTER_FRONT 145 //mm
+#define CENTER_PLIERS 50 //mm
+#define EPSILON 15 //mm
+
+
+class GroupBlocks
+{
+public:
+
+  GroupBlocks(int _xg, int _yg, int _rot, int _xd, int _yd, bool side);
+  GroupBlocks();
+
+  OrientedPoint groupPoint;
+  OrientedPoint depos;
+  OrientedPoint depos_;
+  int rot;
+  OrientedPoint proc_point [9];
+
+private:
+
+};
+#endif
