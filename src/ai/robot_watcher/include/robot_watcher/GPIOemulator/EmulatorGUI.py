6fd93e5aa3cc394b38212c03de2c7bf60de87c88
Alexis CARE <alx.care@gmail.com>
2018-04-30 02:14:26 +0200

    hack lib

--- src/ai/robot_watcher/include/robot_watcher/GPIOemulator/EmulatorGUI.py
+++ src/ai/robot_watcher/include/robot_watcher/GPIOemulator/EmulatorGUI.py
@@ -13,7 +13,7 @@ dictionaryPins = {}
 dictionaryPinsTkinter = {}
 
 # GPIONames=["P8_"+str(i) for i in xrange(1,47)]
-GPIONames=["P8_"+str(i) for i in [1,2,7,8]]
+GPIONames=["P8_"+str(i) for i in [1,2,7,8]] + ["P9_"+str(j) for j in [23,25,27,30,15,12,18,17]]
 
 class App(threading.Thread):
 
@@ -54,6 +54,48 @@ class App(threading.Thread):
         pin8btn.grid(row=1, column=1, padx=(10, 10),pady=(5,5))
 
         dictionaryPinsTkinter["P8_8"] = pin8btn
+#
+        pin9btn = Button(text="P9_23\nOUT=0", command="P9_23", padx ="1px", pady="1px", bd="0px", fg="red", relief="sunken", activeforeground="red")
+        pin9btn.grid(row=0, column=2, padx=(10, 10),pady=(5,5))
+
+        dictionaryPinsTkinter["P9_23"] = pin9btn
+
+        pin10btn = Button(text="P9_25\nOUT=0", command="P9_25", padx ="1px", pady="1px", bd="0px", fg="pink", relief="sunken", activeforeground="pink")
+        pin10btn.grid(row=0, column=3, padx=(10, 10),pady=(5,5))
+
+        dictionaryPinsTkinter["P9_25"] = pin10btn
+
+        pin11btn = Button(text="P9_27\nOUT=0", command="P9_27", padx ="1px", pady="1px", bd="0px", fg="green", relief="sunken", activeforeground="green")
+        pin11btn.grid(row=0, column=4, padx=(10, 10),pady=(5,5))
+
+        dictionaryPinsTkinter["P9_27"] = pin11btn
+
+        pin12btn = Button(text="P9_30\nOUT=0", command="P9_30", padx ="1px", pady="1px", bd="0px", fg="red", relief="sunken", activeforeground="red")
+        pin12btn.grid(row=0, column=5, padx=(10, 10),pady=(5,5))
+
+        dictionaryPinsTkinter["P9_30"] = pin12btn
+
+        pin13btn = Button(text="P9_15\nOUT=0", command="P9_15", padx ="1px", pady="1px", bd="0px", fg="red", relief="sunken", activeforeground="red")
+        pin13btn.grid(row=0, column=6, padx=(10, 10),pady=(5,5))
+
+        dictionaryPinsTkinter["P9_15"] = pin13btn
+
+        pin14btn = Button(text="P9_12\nOUT=0", command="P9_12", padx ="1px", pady="1px", bd="0px", fg="red", relief="sunken", activeforeground="red")
+        pin14btn.grid(row=0, column=7, padx=(10, 10),pady=(5,5))
+
+        dictionaryPinsTkinter["P9_12"] = pin14btn
+
+        pin15btn = Button(text="P9_18\nOUT=0", command="P9_18", padx ="1px", pady="1px", bd="0px", fg="red", relief="sunken", activeforeground="red")
+        pin15btn.grid(row=0, column=8, padx=(10, 10),pady=(5,5))
+
+        dictionaryPinsTkinter["P9_18"] = pin15btn
+
+        pin16btn = Button(text="P9_17\nOUT=0", command="P9_17", padx ="1px", pady="1px", bd="0px", fg="red", relief="sunken", activeforeground="red")
+        pin16btn.grid(row=0, column=9, padx=(10, 10),pady=(5,5))
+
+        dictionaryPinsTkinter["P9_17"] = pin16btn
+
+
 
         # #GND
         # pin6label = Label(text="GND", fg="black")
@@ -479,11 +521,11 @@ class GPIO:
         if channel not in dictionaryPins:
             #if channel is not setup
             raise Exception('GPIO must be setup before used')
-        else:
-            objPin = dictionaryPins[channel]
-            if(objPin.SetMode == "OUT"):
-                #if channel is setup as OUTPUT and used as an INPUT
-                raise Exception('GPIO must be setup as IN')
+        # else:
+        #     objPin = dictionaryPins[channel]
+            # if(objPin.SetMode == "OUT"):
+            #     #if channel is setup as OUTPUT and used as an INPUT
+            #     raise Exception('GPIO must be setup as IN')
 
         objPin = dictionaryPins[channel]
         if(objPin.In == "1"):
