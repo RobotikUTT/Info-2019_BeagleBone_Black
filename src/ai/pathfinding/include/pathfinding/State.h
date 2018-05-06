2836eafd4037248d1b1f55206d32f22819f73d14
Alexis CARE <alx.care@gmail.com>
2018-05-06 15:01:46 +0200

    WIP: first step to integrate pathfinding into BBB

---
+++ src/ai/pathfinding/include/pathfinding/State.h
@@ -0,0 +1,8 @@
+#ifndef PATH_STATE
+#define PATH_STATE
+
+#define OBJ_ADD 0
+#define OBJ_UPDATE 1
+#define OBJ_DELETE 2
+
+#endif
---
+++ src/ai/pathfinding/include/pathfinding/State.py
@@ -0,0 +1,4 @@
+class ObjectManager(object):
+    ADD = 0
+    UPDATE = 1
+    DELETE = 2
