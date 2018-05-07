b4e726efa21a833839a24e3e71fbb1a0069b53a7
Alexis CARE <alx.care@gmail.com>
2018-05-07 10:14:12 +0200

    Typo

--- src/ai/controller/src/OrientedPoint.cpp
+++ src/ai/controller/src/OrientedPoint.cpp
@@ -1,23 +1,20 @@
 #include "controller/OrientedPoint.h"
 
-
-
-
 OrientedPoint::OrientedPoint(int _x, int _y, int _angle){
   x = _x;
-  y=_y;
+  y = _y;
   angle = _angle;
 }
 
 OrientedPoint::OrientedPoint(int _x, int _y){
   x = _x;
-  y=_y;
+  y = _y;
   angle = 0;
 }
 
 OrientedPoint::OrientedPoint(){
   x = 0;
-  y=0;
+  y = 0;
   angle = 0;
 }
 
