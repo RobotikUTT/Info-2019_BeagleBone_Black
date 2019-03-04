3a4f44d6ac3d1ed3adc66a88e8519d8bfdc2395a
Clément de La Bourdonnaye <cle.bourdonnaye@gmail.com>
2019-03-04 11:36:31 +0100

    simulation start signal

--- src/simulation/input_simulation/include/input_simulation/input_simulation_node.hpp
+++ src/simulation/input_simulation/include/input_simulation/input_simulation_node.hpp
@@ -6,15 +6,25 @@
 #include "node_watcher/NodeStatusHandler.hpp"
 
 #include "ai_msgs/NodeStatus.h"
+#include "ai_msgs/Topics.h"
+#include "ai_msgs/StartRobot.h"
+#include "ai_msgs/Side.h"
 
 using ai_msgs::NodeStatus;
+using ros::Publisher;
 
 class InputSimulationNode {
 private:
 	NodeStatusHandler nodes;
+	ros::NodeHandle nh;
 
+	Publisher startSigPub;
+
+	void nodesReady(bool success);
 public:
 	InputSimulationNode();
+
+protected:
 };
 
 #endif
