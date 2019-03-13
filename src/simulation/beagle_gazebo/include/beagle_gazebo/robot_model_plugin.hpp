faab3ed5f0af81450555e5d72b20f09b4a078dff
Clément de La Bourdonnaye <cle.bourdonnaye@gmail.com>
2019-03-13 18:56:59 +0100

    gazebo plugin few changes

--- src/simulation/beagle_gazebo/include/beagle_gazebo/robot_model_plugin.hpp
+++ src/simulation/beagle_gazebo/include/beagle_gazebo/robot_model_plugin.hpp
@@ -3,13 +3,19 @@
 
 #include <functional>
 
+#include "ros/ros.h"
+#include "ros/callback_queue.h"
+#include "ros/subscribe_options.h"
+#include "std_msgs/Float32.h"
+
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
 #include <gazebo/common/common.hh>
 
 #include <ignition/math/Vector3.hh>
 
-// From [http://gazebosim.org/tutorials?tut=plugins_model&cat=write_plugin]
+// From [http://gazebosim.org/tutorials?tut=plugins_model&cat=write_plugin] and
+// [http://gazebosim.org/tutorials?cat=guided_i&tut=guided_i6]
 namespace gazebo {
   class RobotModelPush : public ModelPlugin {
   public:
@@ -18,13 +24,28 @@ namespace gazebo {
     // Called by the world update start event
     void OnUpdate();
 
-  
+    // Handle an incoming message from ROS
+    void OnRosMsg(const std_msgs::Float32ConstPtr &_msg);
+
   private:
+    // ROS helper function that processes messages
+    void QueueThread();
+
     // Pointer to the model
     physics::ModelPtr model;
 
     // Pointer to the update event connection
     event::ConnectionPtr updateConnection;
+
+    // Ros objects
+    std::unique_ptr<ros::NodeHandle> nh;
+    ros::Subscriber rosSub;
+
+    // A ROS callbackqueue that helps process messages
+    ros::CallbackQueue rosQueue;
+
+    // A thread the keeps running the rosQueue
+    std::thread rosQueueThread;
   };
 
   // Register this plugin with the simulator
--- src/simulation/beagle_gazebo/src/robot_model_plugin.cpp
+++ src/simulation/beagle_gazebo/src/robot_model_plugin.cpp
@@ -1,15 +1,59 @@
 #include "beagle_gazebo/robot_model_plugin.hpp"
 
-void gazebo::RobotModelPush::Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/) {
+using namespace gazebo;
+
+void RobotModelPush::Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/) {
     // Store the pointer to the model
     this->model = _parent;
 
+    // Initialize ros, if it has not already been initialized.
+    if (!ros::isInitialized()) {
+      int argc = 0;
+      char **argv = NULL;
+      ros::init(argc, argv, "beagle_gazebo", ros::init_options::NoSigintHandler);
+    }
+
+    // Create our ROS node. This acts in a similar manner to
+    // the Gazebo node
+    this->nh.reset(new ros::NodeHandle("beagle_gazebo"));
+
+    // Create a named topic, and subscribe to it.
+    ros::SubscribeOptions subOptions = ros::SubscribeOptions::create<std_msgs::Float32>(
+        "/STM/GoTo",
+        1,
+        boost::bind(&RobotModelPush::OnRosMsg, this, _1),
+        ros::VoidPtr(),
+        &this->rosQueue
+    );
+    this->rosSub = this->nh->subscribe(subOptions);
+
+    // Spin up the queue helper thread.
+    this->rosQueueThread = std::thread(std::bind(&RobotModelPush::QueueThread, this));
+
     // Listen to the update event. This event is broadcast every
     // simulation iteration.
-    this->updateConnection = event::Events::ConnectWorldUpdateBegin(std::bind(&gazebo::RobotModelPush::OnUpdate, this));
+    this->updateConnection = event::Events::ConnectWorldUpdateBegin(std::bind(&RobotModelPush::OnUpdate, this));
+    
 }
 
-void gazebo::RobotModelPush::OnUpdate() {
-    // Apply a small linear velocity to the model.
-    this->model->SetLinearVel(ignition::math::Vector3d(.3, 0, 0));
+void RobotModelPush::OnUpdate() {
+    // Get model position
+    const ignition::math::Pose3d& pose = this->model->WorldPose();
+
+    // Publish it
+}
+
+/// \brief Handle an incoming message from ROS
+/// \param[in] _msg A float value that is used to set the velocity
+/// of the Velodyne.
+void RobotModelPush::OnRosMsg(const std_msgs::Float32ConstPtr &_msg) {
+  //this->model->SetVelocity(_msg->data);
+}
+
+/// \brief ROS helper function that processes messages
+void RobotModelPush::QueueThread() {
+  static const double timeout = 0.01;
+  while (this->nh->ok()) {
+    this->rosQueue.callAvailable(ros::WallDuration(timeout));
+  }
 }
