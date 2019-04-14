1d1159e48db98bf3c3bf672f3e5544fa917b3b50
Clément de La Bourdonnaye <cle.bourdonnaye@gmail.com>
2019-04-14 19:29:33 +0200

    map parser complete

--- src/ai/vision/include/vision/map.py
+++ src/ai/vision/include/vision/map.py
@@ -1,14 +1,12 @@
-from . import Shape
+from . import Shape, MapObject
 
-from typing import Union, List
+from typing import Union, List, Dict
+from copy import copy, deepcopy
 
 class Offset:
 	def __init__(self, x = 0, y = 0):
 		self.x: int = x
 		self.y: int = y
-
-class Symmetry:
-	pass
 
 class Zone:
 	"""
@@ -17,14 +15,77 @@ class Zone:
 		They are created in order to be provided as action args
 	"""
 	def __init__(self, shape: Shape, name: str):
-		self.shape = shape
 		self.name: str = name
+
+		self.shape = shape
 		self.symmetry: Union[Symmetry, None] = None
 		self.zones: List[Zone] = []
+		self.objects: Dict[MapObject] = []
+		self.wall: bool = False
 	
 	def __str__(self):
 		return  "\n\t".join(
-			["<{}> {}".format(self.name, self.shape)] +
-			("\n".join([z.__str__() for z in self.zones]).split("\n") if len(self.zones) > 0 else [])
+			["<{}> {}{}".format(self.name, self.shape, " wall" if self.wall else "")] +
+			("\n".join([z.__str__() for z in (self.zones + self.objects)]).split("\n") if len(self.zones) + len(self.objects) > 0 else [])
 		)
+
+class Symmetry:
+	"""
+		Symmetry according to an axis (x or y).
+
+		Having axis set to "x" means that objects on one side
+		of the line of equation x = [offset] are cloned on the
+		other side
+	"""
+	def __init__(self):
+		self.axis: str = "x"
+		self.offset: int = 0
+		self.source = ""
+		self.target = ""
+
+	def change_shape_side(self, shape: Shape) -> None:
+		if self.axis == "x":
+			shape.x += (self.offset - shape.x) * 2
+		elif self.axis == "y":
+			shape.y += (self.offset - shape.y) * 2
+		else:
+			raise Exception("unhandled axis : {}".format(self.axis))
+
+	def change_zone_content_side(self, zone: Zone) -> None:
+		# Then on every items
+		for obj in zone.objects:
+			self.change_shape_side(obj.shape)
+		
+		# Then on every subzones
+		for subzone in zone.zones:
+			self.change_shape_side(subzone.shape)
+			self.change_zone_content_side(subzone)
+		
+
+	def apply(self, zone: Zone):
+		"""
+			Duplicate zone content into it's symmetrical zone,
+			then return
+		"""
+		# Make two copy of the shape
+		original = copy(zone)
+		duplicate = deepcopy(zone)
+		self.change_zone_content_side(duplicate)
+
+		# Reset original shape
+		zone.__init__(original.shape, original.name)
+
+		# Set new names
+		original.name = self.source
+		duplicate.name = self.target
+
+		# Only original zone keep wall as it's shape is kept
+		if original.wall:
+			zone.wall = True
+
+			original.wall = False
+			duplicate.wall = False
+
+		# Add subzones
+		zone.zones = [original, duplicate]
 
--- src/ai/vision/include/vision/object.py
+++ src/ai/vision/include/vision/object.py
@@ -1,9 +1,19 @@
+from . import Shape
+
 from typing import List, Dict
 
 from action_manager import Argumentable
-from copy import copy
+from copy import copy, deepcopy
 
 class MapObjectAction(Argumentable):
+	"""
+		Action bound to an object on the field.
+		The robot might choose to proceed with this action if
+		it is a viable option.
+
+		Some action bound to the same object can be incompatible
+		(example: move an object to 2 different places)
+	"""
 	def __init__(self, file: str):
 		super().__init__()
 
@@ -14,9 +24,14 @@ class MapObjectAction(Argumentable):
 
 
 class MapObjectArgument:
-	def __init__(self, name: str, type: str):
-		super().__init__()
+	"""
+		Argument that can be given to an generic object.
 
+		Example : a ball object is defined in objects.xml, but it might
+		have different parameters depending on it's color, so an argument
+		color is created.
+	"""
+	def __init__(self, name: str, type: str):
 		self.name = name
 		self.type = type
 		self.bound: Dict[str, Argumentable] = {}
@@ -32,10 +47,14 @@ class MapObjectArgument:
 
 		return self.bound[value]
 	
+	def get_value(self, name: str) -> Argumentable:
+		return self.bound[name]
+
 	def __str__(self):
 		return "<{}: {}> {}" \
 			.format(self.name, self.type,
-			 	", ".join(["{} ({})".format(a, self.bound[a].__str__()) for a in self.bound])
+			 	", ".join(["{}".format(a) for a in self.bound])
+			 	#", ".join(["{} ({})".format(a, self.bound[a].__str__()) for a in self.bound])
 			)
 
 
@@ -45,23 +64,26 @@ class MapObject(Argumentable):
 		to deal with and some properties
 	"""
 	def __init__(self):
+		super().__init__()
+
 		self.name = ""
-		self.shape = None
+		self.shape: Shape = Shape()
 		self.actions: List[MapObjectAction] = []
 		self.args: Dict[str, MapObjectArgument] = {}
 	
 	def extends(self, obj: 'MapObject'):
-		# Make a copy of the shape
+		# Make a copy of the shape and args
 		self.shape = copy(obj.shape)
-		
-		# TODO clone args
-		self.args = copy(obj.args)
+		self.args = deepcopy(obj.args)
 	
+	def clone(self):
+		return deepcopy(self)
+
 	def __str__(self):
 		return "\n\t".join(
-				["[{}] {}".format(self.name, self.shape)] +
-				[a.__str__() for a in self.actions] +
-				[self.args[a].__str__() for a in self.args]
-			)
+			["[{}] {} {}".format(self.name, self.shape, super().__str__())] +
+			[a.__str__() for a in self.actions] +
+			[self.args[a].__str__() for a in self.args]
+		)
 
 
