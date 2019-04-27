98d39cfee08b53bb2ab343a104c696fe6e0c93be
Clément de La Bourdonnaye <cle.bourdonnaye@gmail.com>
2019-04-27 22:52:56 +0200

    integrated python-can code inside can_interface node (untested)

--- src/interface/can_interface/include/can_interface/io_elements.py
+++ src/interface/can_interface/include/can_interface/io_elements.py
@@ -3,25 +3,32 @@ from can_msgs.msg import Frame
 
 from interface_description.msg import InterfaceTopics as Topics
 
+# Can library
+import can
+
+import time
+
+from typing import Dict, List
+
 class Param:
 	def __init__(self, name):
-		self.name = name
-		self.size = -1
-		self.byte_start = 0
+		self.name: str = name
+		self.size: int = -1
+		self.byte_start: int = 0
 	
 	def __str__(self):
 		return "{}[{}-{}]".format(self.name, self.byte_start, self.byte_start + self.size - 1)
 
 class IOElement:
 	def __init__(self, settings, params, interface):
-		self.settings = settings
-		self.params = []
+		self.settings: dict = settings
+		self.params: List[Param] = []
 
 		# Set settings topic to real topic
 		self.settings["topic"] = getattr(Topics, self.settings["topic"])
 
 		# Convert params XML list to param list
-		current_offset = 1
+		current_offset: int = 1
 
 		for param in params:
 			current = Param(
@@ -49,7 +56,7 @@ class InputElement(IOElement):
 	def __init__(self, settings, params, interface):
 		IOElement.__init__(self, settings, params, interface)
 
-		self.publisher = Publisher(self.settings["topic"], self.message, queue_size=10)
+		self.publisher: Publisher = Publisher(self.settings["topic"], self.message, queue_size=10)
 		interface.subscribe(self.settings["frame"], self)
 	
 	def on_can_message(self, frame):
@@ -65,7 +72,7 @@ class InputElement(IOElement):
 
 
 	# Retrieve param data from frame data with binary operations
-	def get_param_value(self, frame, param):
+	def get_param_value(self, frame: can.Message, param: Param):
 		value = frame.data[param.byte_start + param.size - 1]
 
 		for index in range(param.size - 1):
@@ -86,34 +93,39 @@ class OutputElement(IOElement):
 		IOElement.__init__(self, settings, params, interface)
 
 		self.device_id = id
-		self.interface = interface
 		self.subscriber = Subscriber(self.settings["topic"], self.message, self.on_ros_message)
-	
+
+		self.command_type: int = getattr(Frame, self.settings["frame"])
+
+		self.bus = can.interface.Bus("can1")
+
 	def on_ros_message(self, message):
-		frame = Frame()
+		frame: can.Message = can.Message()
 
 		# Basic settings
-		frame.header.stamp = Time.now()
-		frame.header.frame_id = "/ros_can/interface/"
-		frame.is_rtr = 0
-		frame.is_error = 0
-		frame.is_extended = 0
-		frame.dlc = 1
+		frame.stamp = Time.now()
+		frame.is_remote_frame = 0 #
+		frame.is_error_frame = 0 #
+		frame.extended_id = 0 #
+		frame.dlc = 1 #
 
-		frame.id = self.device_id
+		frame.arbitration_id = self.device_id
 
-		data_array = [0] * 8
+		data_array: List[int] = [0] * 8
 
 		# Add params
 		for param in self.params:
 			self.put_param_value(data_array, message, param)
 		
+		# Set frame type
+		data_array[0] = self.command_type
+
 		frame.data = bytes(data_array)
 
 		# Send frame to ros
-		self.interface.can_publisher.publish(frame)
+		self.bus.send(frame)
 
-	def put_param_value(self, data_array, msg, param):
+	def put_param_value(self, data_array: List[int], msg, param: Param):
 		if param.size == 1:
 			data_array[param.byte_start] = getattr(msg, param.name)
 		elif param.size == 2:
