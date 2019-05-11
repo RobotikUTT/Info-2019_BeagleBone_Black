98ce3ff2bba7fd07d745cbee98398d50e0e9bc21
Clément de La Bourdonnaye <cle.bourdonnaye@gmail.com>
2019-05-11 12:06:21 +0200

    split xml_class_parser unit tests, removed unused pathfinding

--- src/ai/pathfinding/include/pathfinding/mesh/__init__.py
+++
@@ -1,5 +0,0 @@
-from .face import Face, GhostFace, MeshFace
-from .vertex import Vertex, GhostVertex, MeshVertex
-from .edge import Edge, GhostEdge, MeshEdge
-
-from .mesh_solid import MeshSolid
--- src/ai/pathfinding/include/pathfinding/mesh/tools/__init__.py
+++
@@ -1,2 +0,0 @@
-from .merge_mesh import merge_meshes
-from .divide_and_conquer import divide_and_conquer
