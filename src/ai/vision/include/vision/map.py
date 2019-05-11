45e5bec74331cda6d41b1cf5ee68db53e06d868d
Clément de La Bourdonnaye <cle.bourdonnaye@gmail.com>
2019-05-11 13:57:42 +0200

    map parsing and todo list

--- src/ai/vision/include/vision/map.py
+++ src/ai/vision/include/vision/map.py
@@ -1,47 +1,113 @@
-from . import Shape, MapObject
+from . import MapObject
+from .shape import Circle, Rect
 
 from typing import Union, List, Dict
 from copy import copy, deepcopy
 
 from xml_class_parser import Parsable, Bind, BindList
 
+@Parsable(
+	name="offset",
+	attributes={
+		"x": int,
+		"y": int
+	},
+	children = [
+		Bind(to="zones", RectZone),
+		Bind(to="zones", CircleZone),
+		BindList(to="zones", Offset)
+	]
+)
 class Offset:
 	def __init__(self, x = 0, y = 0):
 		self.x: int = x
 		self.y: int = y
-
-@Parsable(
-	name="zone",
-	attributes={
-		"name": Bind(type=str, mandatory=True)
-	},
-	extends = Shape,
-	children = [
-		Bind(to="shape", Shape),
-		BindList(to="zones", Offset)
-	]
-)
-class Zone:
-	"""
-		Define a zone in which some actions might take place.
-		
-		They are created in order to be provided as action args
-	"""
-	def __init__(self, shape: Shape, name: str):
-		self.name: str = name
-
-		self.shape = shape
-		self.symmetry: Union[Symmetry, None] = None
-		self.zones: List[Zone] = []
-		self.objects: Dict[MapObject] = []
-		self.wall: bool = False
+		self.zones: List[Offset] = []
 	
+	def __apply_offset(self, obj: Offset):
+		obj.x += self.x
+		obj.y += self.y
+		return obj
+
+	def __parsed__(self, context):
+		# flatten offsets
+		max = len(self.zones)
+		i = 0
+		while i < max:
+			zone = self.zones[i]
+
+			if type(zone) == Offset:
+				self.zones = self.zones[0:i] + \
+					zone.zones.map(self.__apply_offset) + self.zones[i+1:]
+			else:
+				i += 1
+
+
+class Zone(Offset):
+	"""
+		Define a zone in the map, that is either a "virtual" zone,
+		either a physical blocking zone
+	"""
+	def __init__(self):
+		super().__init__()
+		self.name: str = ""
+		self.symmetry: Union[Symmetry, None] = None
+		self.blocking: bool = False
+	
+	def __parsed__(self, ctx):
+		# first flatten offsets
+		super().__parsed__(ctx)
+
+		# apply symmetry
+		if self.symmetry is not None:
+			self.symmetry.apply(self)
+
+
 	def __str__(self):
 		return  "\n\t".join(
 			["<{}> {}{}".format(self.name, self.shape, " wall" if self.wall else "")] +
 			("\n".join([z.__str__() for z in (self.zones + self.objects)]).split("\n") if len(self.zones) + len(self.objects) > 0 else [])
 		)
+@Parsable(
+	name="rect-zone",
+	attributes = {
+		"x": int,
+		"y": int,
+		"width": int,
+		"height": int,
+		"blocking": bool
+		"name": Bind(type=str, mandatory=True)
+	},
+	children = [
+		BindList(to="zones", RectZone),
+		BindList(to="zones", CircleZone),
+		BindList(to="zones", Offset)
+	]
+)
+class RectZone:
+	pass
 
+@Parsable(
+	name="circle-zone",
+	attributes = {
+		"x": int,
+		"y": int,
+		"radius": int,
+		"blocking": bool
+		"name": Bind(type=str, mandatory=True)
+	},
+	children = [
+		BindList(to="zones", RectZone),
+		BindList(to="zones", CircleZone),
+		BindList(to="zones", Offset)
+	]
+)
+class RectZone:
+	pass
+
+@Parsable(
+	name="symmetry"
+)
 class Symmetry:
 	"""
 		Symmetry according to an axis (x or y).
